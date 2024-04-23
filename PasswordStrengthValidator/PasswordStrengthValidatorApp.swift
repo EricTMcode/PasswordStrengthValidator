fac32f81de732c05cb0e693d1d6cc792252d608b
Eric <ebagnaro@gmail.com>
2024-04-23 13:11:35 +0200

    Initial Commit

---
+++ PasswordStrengthValidator/PasswordStrengthValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordStrengthValidatorApp.swift
+//  PasswordStrengthValidator
+//
+//  Created by Eric on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PasswordStrengthValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
