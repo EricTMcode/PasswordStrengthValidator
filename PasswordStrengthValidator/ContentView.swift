b415bdd41aa8255b849a4c3150edc23c6c87d71d
Eric <ebagnaro@gmail.com>
2024-04-23 13:28:27 +0200

    - Initial commit

--- PasswordStrengthValidator/ContentView.swift
+++ PasswordStrengthValidator/ContentView.swift
@@ -7,18 +7,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum PasswordStrength: String {
+    case weak = "Weak"
+    case moderate = "Moderate"
+    case strong = "Strong"
+}
+
+struct Password {
+    var value: String
+    
+    func validate() -> PasswordStrength {
+        let passwordLength = value.count
+        
+        switch passwordLength {
+        case 0..<6:
+            return .weak
+        case 6..<10:
+            return .moderate
+        case 10...:
+            return .strong
+        default:
+            return .weak
         }
-        .padding()
+    }
+}
+
+struct PasswordStrengthView: View {
+    let passwordStrength: PasswordStrength
+    
+    var passwordStrengthColor: Color {
+        switch passwordStrength {
+        case .weak:
+                .red
+        case .moderate:
+                .orange
+        case .strong:
+                .green
+        }
+    }
+    
+    var body: some View {
+        Text(passwordStrength.rawValue)
+            .padding(5)
+            .background(passwordStrengthColor)
+    }
+}
+
+struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password = Password(value: "")
+    
+    private var passwordStrength: PasswordStrength {
+        password.validate()
+    }
+    
+    var body: some View {
+        Form {
+            TextField("User name", text: $username)
+            SecureField("Password", text: $password.value)
+            Button("Signup") {
+                
+            }
+            
+            PasswordStrengthView(passwordStrength: passwordStrength)
+                .opacity(password.value.isEmpty ? 0 : 1.0)
+        }
+        .navigationTitle("Signup")
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+    }
 }
